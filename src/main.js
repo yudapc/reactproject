1c73d79f1a1f50319e6c87dd170523e0a7f46d78
yudapc <yudapc@gmail.com>
2017-02-18 10:06:29 +0700

    added webpack development server

---
+++ src/main.js
@@ -0,0 +1,2 @@
+let docBody = "Hallo ES6!";
+document.write(docBody);
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ module.exports = {
     app: ["./src/main.js"]
   },
   output: {
-    path: "./public/assets/",
+    // path: "./public/assets/",
+    path: __dirname + "/dist/js", // or path: path.join(__dirname, "dist/js"),
     publicPath: "/assets/",
     filename: "bundle.js"
   }
